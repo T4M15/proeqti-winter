e110b9aaf775ebb17cf31048329189c90a060564
T4M15 <temikotemi20@gmail.com>
2026-01-12 22:09:26 +0400

    pirveli naxevari tamashi:ricxvobana

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+import time
+
+start_game = input("გინდა თამაშის დაწყება? (კი/არა): ")
+
+if start_game != "კი":
+    exit()
+
+print("level 1 = 1-100")
+print("level 2 = 1-200")
+print("level 3 = 1-300")
+
+choose_level = int(input("აირჩიე სირთულე: (1/2/3): "))
+
+if choose_level == 1:
+    max_number = 100
+elif choose_level == 2:
+    max_number = 200
+elif choose_level == 3:
+    max_number = 300
+else:
+    print("Wrong level")
+    exit()
